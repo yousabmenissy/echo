27edf365cd5f0c7aa57b509550084f2e6779f7db
yousabbbbb <yousabmenissy@gmail.com>
2024-11-18 18:18:22 +0200

    House Keeping

--- echo.s
+++ echo.s
@@ -2,7 +2,6 @@
     This program is a very simple clone of the echo command.
     it loops through all the arguments a print them with spaces in between.
 */
-.include "lib/strlen.s"
 .include "lib/sys_write.s"
 .include "lib/sys_exit.s"
 
@@ -10,40 +9,35 @@
 usage: .string "Usage: echo [ arg... ]"
 usage_len: .quad . - usage
 
+space: .string " "
+
 .section .text
 .global _start
 _start:
     movq %rsp, %rbp
-    subq $8, %rsp
 
-    movq (%rbp), %rbx       # rbx hold the number of the arguments(i.e. argc)
-    movq %rbx, -8(%rbp)     # save argc to the stack
-
+    movq (%rbp), %r8      # r8 = argc
 section0:
-    cmpq $1, %rbx
-    je exit_error           # if argc == 1, jump to exit_error
+    cmpq $1, %r8
+    je exit_usage         # if argc == 1, jump to exit_usage
 
-    movq %rbp, %rbx         # copy of the current value of rbp
-    addq $8, %rbx           # skip argv[0], the name of the file
-
+    movq %rbp, %rbx
+    addq $8, %rbx
 section1:
 .LP0:
-    addq $8, %rbx           # rbx = argv[1]
-    movq (%rbx), %rdi
-    call strlen             # strlen calculate the length of the argument
-    write $1, (%rdi), %rax  # the sys_write file contain the write, writes, writeln macros
-    writes $1
+    addq $8, %rbx
+    movq (%rbx), %r9
+    print (%r9)
+    write $1, space(%rip), $1
 
-    decq -8(%rbp)           # --argc
-    cmpq $1, -8(%rbp)
-    jg .LP0                 # if argc > 1, repeat the loop
+    decq %r8
+    cmpq $1, %r8
+    jg .LP0               # if argc > 1, repeat the loop
 
-# write new line and exit
-    writeln $1
-    exit $0                 # the file sys_exit contain the exit macro
+exit_success:
+    newline $1
+    exit $0
 
-# will only run if the program was run without arguments(i.e. argc == 1)
-exit_error:
-    write $1, usage(%rip), usage_len
-    writeln $1
+exit_usage:
+    writeln $1, usage(%rip), usage_len
     exit $-1
--- lib/sys_write.s
+++ lib/sys_write.s
@@ -1,6 +1,13 @@
 .section .data
-newline: .ascii "\n"
-spacechar: .ascii " "
+newln: .ascii "\n"
+.macro newline fd
+    movq \fd, %rdi
+    leaq newln(%rip), %rsi
+    movq $1, %rdx
+    movq $1, %rax
+    syscall
+.endm
+
 .macro write fd, str, len
     movq \fd, %rdi
     leaq \str, %rsi
@@ -9,18 +16,52 @@ spacechar: .ascii " "
     syscall
 .endm
 
-.macro writeln fd
-    movq $1, %rax
-    movq \fd, %rdi
-    leaq newline(%rip), %rsi
-    movq $1, %rdx
+.macro writeln fd, str, len
+    write \fd, \str, \len
+    newline \fd
+.endm
+
+.macro print str
+    leaq \str, %rdi
+    call strlen
+    write $1, \str, %rax
+.endm
+
+.macro println str
+    leaq \str, %rdi
+    call strlen
+    writeln $1, \str, %rax
+.endm
+
+.macro fatalln str, status
+    println \str
+    movq \status, %rdi
+    movq $60, %rax
     syscall
 .endm
 
-.macro writes fd
-    movq $1, %rax
-    movq \fd, %rdi
-    leaq spacechar(%rip), %rsi
-    movq $1, %rdx
-    syscall
-.endm
+.section .text
+# rdi holds the string pointer
+.type strlen, @function
+strlen:
+    pushq %rbp
+    movq %rsp, %rbp
+
+continueSL0:
+    pushq %rbx
+    movq $0, %rbx
+
+    movq %rdi, %rsi
+.LPSL0:
+    lodsb
+    cmpb $0, %al
+    je continueSL1
+    incq %rbx
+    jmp .LPSL0
+
+continueSL1:
+    movq %rbx, %rax
+    popq %rbx
+
+    leave
+    ret
